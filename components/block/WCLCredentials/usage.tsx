fd1167b5ee1f0d276a684b794d8959068bbc0955
Liroo <liroo.pierre@gmail.com>
2024-08-13 02:01:38 +0200

    fix build

--- components/block/WCLCredentials/usage.tsx
+++ components/block/WCLCredentials/usage.tsx
@@ -2,9 +2,9 @@ export default function WCLCredentialsUsage() {
   return (
     <div>
       <p>
-        To avoid hitting my own rate limits, I’m asking for your ClientID and
-        ClientSecret. Don’t worry, these credentials are stored securely in your
-        browser, and I don’t have access to them (I'm generating a token to
+        To avoid hitting my own rate limits, I'm asking for your ClientID and
+        ClientSecret. Don't worry, these credentials are stored securely in your
+        browser, and I don't have access to them (I'm generating a token to
         communicate with Warcraft Logs directly from the browser).
         <br />
         <br />
