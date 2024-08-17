dcc430133c7fdc1395ddeb0740bd4e082bb128d5
Liroo <liroo.pierre@gmail.com>
2024-08-17 14:52:15 +0200

    Disable all form in case of no bearer token

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,16 @@ import BossSelect from 'components/block/bossSelect';
 import LogsSelect from 'components/block/logsSelect';
 import Roster from 'components/block/roster';
 import CoreLogEventScreen from 'components/core/logEventScreen';
+import { selectAuthBearerToken } from 'flux/auth/selector';
+import { useAppSelector } from 'flux/hooks';
 
 export default function Home() {
+  const bearerToken = useAppSelector(selectAuthBearerToken);
+
   return (
-    <main className="my-[16px] flex flex-col gap-[16px]">
+    <main
+      className={`my-[16px] flex flex-col gap-[16px] ${bearerToken ? '' : 'pointer-events-none opacity-50'}`}
+    >
       <Roster />
 
       <BossSelect />
